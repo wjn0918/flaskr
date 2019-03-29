8299596cb128fd2bd035d9ce936be78ea0f2ea15
wjn0918 <wjn_0918@163.com>
2019-03-29 17:24:50 +0800

    初始化数据库sql

--- schema.sql
+++ schema.sql
@@ -2,15 +2,15 @@ drop table if exists user;
 drop table if exists post;
 
 create table user(
-  id int auto_increment,
+  id integer auto_increment,
   username varchar(10) not null,
   password varchar(20) not null,
   primary key(id)
 );
 
 create table post(
-  id int auto_increment,
-  author_id int not null,
+  id integer auto_increment,
+  author_id integer not null,
   created timestamp null default current_timestamp on update current_timestamp,
   title varchar(20) not null,
   body varchar(255) not null,
