b90feca8872583c4f717dedfd10fbf23e804d3a1
wjn0918 <wjn_0918@163.com>
2019-03-29 17:24:50 +0800

    1

--- __init__.py
+++ __init__.py
@@ -30,9 +30,17 @@ def create_app(test_config=None):
         return 'Hello, World!'
     
 
-
     from . import db
     db.init_app(app)
+    
+    #认证蓝图将包括注册新用户、登录和注销视图。
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
