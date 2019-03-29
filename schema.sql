614a4ebed14fadfd93825f3c4548acd5a33c4a7d
wjn0918 <wjn_0918@163.com>
2019-03-29 19:38:37 +0800

    初始化数据库sql语句

--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
 drop table if exists user;
 drop table if exists post;
-
+#创建用户信息表
 create table user(
   id integer auto_increment,
   username varchar(10) not null,
   password varchar(20) not null,
   primary key(id)
 );
-
+#创建用户博客表
 create table post(
   id integer auto_increment,
   author_id integer not null,
