7dd89fedebc513dfa99b37120319271a7380197f
wjn0918 <wjn_0918@163.com>
2019-03-29 21:47:17 +0800

    初始化数据库

--- schema.sql
+++ schema.sql
@@ -1,18 +1,17 @@
 drop table if exists user;
 drop table if exists post;
-#创建用户信息表
+
 create table user(
   id integer auto_increment,
-  username varchar(10) not null,
-  password varchar(20) not null,
+  username text not null,
+  password text not null,
   primary key(id)
 );
-#创建用户博客表
 create table post(
   id integer auto_increment,
   author_id integer not null,
   created timestamp null default current_timestamp on update current_timestamp,
-  title varchar(20) not null,
-  body varchar(255) not null,
+  title text not null,
+  body text not null,
   primary key(id)
 );
